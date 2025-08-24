876c664297b1f64899f5304797439353a94ad8da
Jugal Lachhwani <144345250+Jugal-lachhwani@users.noreply.github.com>
2025-08-24 22:19:31 +0530

    Time: 51 ms (76.22%), Space: 21.7 MB (68.25%) - LeetHub

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestSubarray(self, arr: List[int]) -> int:
+        zero = False
+        ones1 = 0
+        ones2 = 0
+        totalones = 0
+
+        for i in arr:
+            if zero == False and i == 1:
+                ones1 += 1
+            if zero == True and i == 1:
+                ones2 += 1
+            if i == 0 and zero == True:
+                totalones = max(totalones,ones1 + ones2)
+                ones1 = ones2
+                ones2 = 0
+            if i == 0 and zero == False:
+                zero = True
+                totalones = max(totalones,ones1 + ones2)
+        if zero == False:
+            return ones1 - 1
+        return max(totalones,ones2+ones1)
+            

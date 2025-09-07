b518d4f92a4b8907341098ca75c2a70586fbdc72
Jugal Lachhwani <144345250+Jugal-lachhwani@users.noreply.github.com>
2025-09-07 12:05:27 +0530

    Time: 3 ms (37.01%), Space: 18 MB (13.14%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,21 +1,15 @@
 class Solution:
     def simplifyPath(self, p: str) -> str:
-        if len(p) == 1:
-            return p
         if p[len(p) - 1] == '/':
             p = p[0:len(p) -1]
         l = re.findall('[a-z.A-Z_1-9]+',p)
-        p = ''
-        l1 = []
         i = 0
         while i < len(l):
             if l[i] == '.' or l[i] == '..':
                 if l[i] == '..':
                     if i > 0:
-                        # print('before pop =',l,i)
                         l.pop(i-1)
                         i = i-1
-                        # print('after pop =',l,i)
                 l.pop(i)
             else:
                 i += 1

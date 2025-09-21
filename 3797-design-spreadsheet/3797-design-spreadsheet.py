6dc469488dcd2255a750b1086b0c5e6ee670d0b6
Jugal Lachhwani <144345250+Jugal-lachhwani@users.noreply.github.com>
2025-09-21 18:43:21 +0530

    Time: 284 ms (28.97%), Space: 18.7 MB (67.31%) - LeetHub

--- 3797-design-spreadsheet/3797-design-spreadsheet.py
+++ 3797-design-spreadsheet/3797-design-spreadsheet.py
@@ -1,20 +1,38 @@
 class Spreadsheet(object):
 
     def __init__(self, rows):
-        self.mpp = {}
+        self.l = l = [[0] * 26 for _ in range(rows + 1)]
 
     def setCell(self, cell, value):
-        self.mpp[cell] = value
+        col = ord(cell[0]) - 65
+        row = int(cell[1:])
+        self.l[row][col] = value
+        
 
     def resetCell(self, cell):
-        self.mpp[cell] = 0
+        col = ord(cell[0]) - 65
+        row = int(cell[1:])
+
+        self.l[row][col] = 0
+
+        
 
     def getValue(self, formula):
-        formula = formula[1:]
-        for i in range(len(formula)):
-            if formula[i] == '+':
-                s1, s2 = formula[:i], formula[i+1:]
-                left = self.mpp.get(s1, 0) if s1[0].isupper() else int(s1)
-                right = self.mpp.get(s2, 0) if s2[0].isupper() else int(s2)
-                return left + right
-        return 0
+        s = formula[1:].split('+')
+        sum = 0
+        for i in s:
+            if not i[0].isnumeric():
+                col = ord(i[0]) - 65
+                row = int(i[1:])
+                sum += self.l[row][col]
+            else:
+                sum+=int(i)
+        return sum
+        
+
+
+# Your Spreadsheet object will be instantiated and called as such:
+# obj = Spreadsheet(rows)
+# obj.setCell(cell,value)
+# obj.resetCell(cell)
+# param_3 = obj.getValue(formula)

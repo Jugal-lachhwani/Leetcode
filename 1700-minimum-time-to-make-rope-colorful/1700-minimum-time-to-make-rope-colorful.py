d6c6f3d0d38cc02b835115155d596b41b57e1baf
Jugal Lachhwani <144345250+Jugal-lachhwani@users.noreply.github.com>
2025-11-03 23:24:45 +0530

    Time: 157 ms (25.58%), Space: 26.4 MB (62.73%) - LeetHub

---
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minCost(self, c: str, t: List[int]) -> int:
+        r = 0
+        i = 0
+        while i < len(c):
+            b = c[i]
+            # s = set()
+            j = i+1
+            # rep = False
+            m = t[i]
+            con = 0
+            while j < len(c) and c[j] == c[i]:
+                # rep = True
+                # s.add(t[j])
+                # s.add(t[i])
+                con += 1
+                if con < 2:
+                    ma = max(t[j],m)
+                    m = min(t[j],m)
+                else:
+                    m = min(t[j],ma)
+                    ma = max(t[j],ma)
+                r+=m
+                j+=1
+            i = j
+        return r

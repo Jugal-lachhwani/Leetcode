ea4e7f17884cd3751c3e60383d6a4931668daf35
Jugal Lachhwani <144345250+Jugal-lachhwani@users.noreply.github.com>
2026-01-08 23:45:24 +0530

    Time: 451 ms (18.52%), Space: 31.7 MB (6.89%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minDays(self, arr: List[int], m: int, k: int) -> int:
+        def flowerbed(arr,mid,k,m):
+            k_c = 0
+            for i in arr:
+                if i - mid <= 0:
+                    k_c += 1
+                    if k == k_c:
+                        k_c = 0
+                        m-=1
+                else:
+                    k_c = 0
+            return m <= 0
+        if k * m > len(arr):
+            return -1
+        l = 0
+        r = max(arr)
+        min_num = max(arr)
+        while l <= r:
+            mid = (l+r) // 2
+            b = flowerbed(arr,mid,k,m)
+            if b == True:
+                min_num = min(mid,min_num)
+                r = mid - 1
+            else:
+                l = mid + 1
+        return min_num

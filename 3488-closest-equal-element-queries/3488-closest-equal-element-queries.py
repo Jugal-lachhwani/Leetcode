d8c5931d1dd729ca91682f6360a25f4dc658e46d
Jugal Lachhwani <144345250+Jugal-lachhwani@users.noreply.github.com>
2026-04-17 12:13:16 +0530

    Time: 363 ms (53.91%), Space: 53.7 MB (86.72%) - LeetHub

---
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.py
@@ -0,0 +1,36 @@
+class Solution:
+    def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        ans = [0] * len(queries)
+        d = {}
+        n = len(nums)
+        for i in range(n):
+            if nums[i] not in d:
+                d[nums[i]] = []
+            d[nums[i]].append(i)
+        for i in range(len(queries)):
+            target = queries[i]
+
+            if nums[target] not in d or len(d[nums[target]]) == 1:
+                ans[i] = -1
+                continue
+            arr = d[nums[target]]
+            l = 0
+            r = len(arr) - 1
+            # while l < r:
+            #     mid = l + r // 2
+            #     if arr[mid] == target:
+            #         break
+            #     elif arr[mid] < target:
+            #         l = mid + 1
+            #     else:
+            #         r = mid - 1
+            mid = bisect_left(arr, target)
+            left = arr[mid - 1] if mid > 0 else arr[-1]
+            right = arr[mid + 1] if mid < len(arr) - 1 else arr[0]
+
+            def dist(a, b):
+                return min(abs(a - b), n - abs(a - b))
+
+
+            ans[i] = min(dist(target, left), dist(target, right))
+        return ans

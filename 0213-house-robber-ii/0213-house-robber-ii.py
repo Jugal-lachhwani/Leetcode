652ddf5a4edcc3e1e9dbb8038885aafdfd9bac85
Jugal Lachhwani <144345250+Jugal-lachhwani@users.noreply.github.com>
2026-04-18 12:21:01 +0530

    Time: 0 ms (100%), Space: 19.3 MB (73.19%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def rob(self,nums):
+        n = len(nums)
+        if n <= 1:
+            return nums[0]
+        return max(self.findMax(nums[0:n-1]), self.findMax(nums[1:n])) 
+    def findMax(self, nums) -> int:
+        n = len(nums)
+        dp = [-1] * n
+        max_sum = float("-inf")
+        prev1, prev2, prev3 = 0,0,0
+        for i in range(n):
+            if i <= 1:
+                curr = nums[i]
+                max_sum = max(max_sum, curr)
+                prev3 = prev2
+                prev2 = prev1
+                prev1 = curr
+                continue
+            if i <= 2:
+                curr = prev2 + nums[i]
+                prev3 = prev2
+                prev2 = prev1
+                prev1 = curr
+                max_sum = max(max_sum, curr)
+                continue         
+            first =  prev2 + nums[i]
+            second = prev3 + nums[i]
+            curr = max(first,second)
+            prev3 = prev2
+            prev2 = prev1
+            prev1 = curr
+            max_sum = max(max_sum, curr)
+        return max_sum

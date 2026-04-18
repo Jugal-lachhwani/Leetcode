1b06724230fe7497ce7fffb0fbd70c74dd6a0fd8
Jugal Lachhwani <144345250+Jugal-lachhwani@users.noreply.github.com>
2026-04-18 11:53:01 +0530

    Time: 0 ms (100%), Space: 19.2 MB (67.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,18 +3,27 @@ class Solution:
         n = len(nums)
         dp = [-1] * n
         max_sum = float("-inf")
-        
+        prev1, prev2, prev3 = 0,0,0
         for i in range(n):
             if i <= 1:
-                dp[i] = nums[i]
-                max_sum = max(max_sum, dp[i])
+                curr = nums[i]
+                max_sum = max(max_sum, curr)
+                prev3 = prev2
+                prev2 = prev1
+                prev1 = curr
                 continue
             if i <= 2:
-                dp[i] = dp[i-2] + nums[i]
-                max_sum = max(max_sum, dp[i])
+                curr = prev2 + nums[i]
+                prev3 = prev2
+                prev2 = prev1
+                prev1 = curr
+                max_sum = max(max_sum, curr)
                 continue         
-            first =  dp[i-2] + nums[i]
-            second = dp[i-3] + nums[i]
-            dp[i] = max(first,second)
-            max_sum = max(max_sum, dp[i])
+            first =  prev2 + nums[i]
+            second = prev3 + nums[i]
+            curr = max(first,second)
+            prev3 = prev2
+            prev2 = prev1
+            prev1 = curr
+            max_sum = max(max_sum, curr)
         return max_sum

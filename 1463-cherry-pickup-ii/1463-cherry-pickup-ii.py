16c90eacfa5957e54d351136395610ac236e0694
Jugal Lachhwani <144345250+Jugal-lachhwani@users.noreply.github.com>
2026-04-19 11:30:33 +0530

    Time: 1146 ms (33.47%), Space: 20 MB (84.38%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -1,30 +1,33 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
-        # 3D DP table
         n = len(grid)
         m = len(grid[0])
-        dp = [[[0 for _ in range(m)] for _ in range(m)] for _ in range(n)]
-        
-        # Base case: last row
+        next_dp = [[0 for i in range(m)] for j in range(m)]
+        cur_dp = [[0 for i in range(m)] for j in range(m)]
         for j1 in range(m):
             for j2 in range(m):
                 if j1 == j2:
-                    dp[n-1][j1][j2] = grid[n-1][j1]
+                    next_dp[j1][j2] = grid[n-1][j1]
                 else:
-                    dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2]
-        
-        # Fill DP table bottom-up
-        for i in range(n-2, -1, -1):
+                    next_dp[j1][j2] = grid[n-1][j1] + grid[n-1][j2]
+        print(next_dp)
+
+        for i in range(n-2,-1,-1):
             for j1 in range(m):
                 for j2 in range(m):
-                    maxi = -10**9
-                    curr = grid[i][j1] if j1 == j2 else grid[i][j1] + grid[i][j2]
-                    for dj1 in [-1, 0, 1]:
-                        for dj2 in [-1, 0, 1]:
-                            newJ1, newJ2 = j1 + dj1, j2 + dj2
-                            if 0 <= newJ1 < m and 0 <= newJ2 < m:
-                                maxi = max(maxi, curr + dp[i+1][newJ1][newJ2])
-                            else:
-                                maxi = max(maxi, -10**9)
-                    dp[i][j1][j2] = maxi
-        return dp[0][0][m-1]    
+                    if j1 == j2:
+                        cur_sum = grid[i][j1]
+                    else:
+                        cur_sum = grid[i][j1] + grid[i][j2]
+                    max_sum = float('-inf')
+                    for ind1 in (1,0,-1):
+                        for ind2 in (1,0,-1):
+                            newj1,newj2 = j1 + ind1, j2 + ind2
+                            if 0 <= newj1 < m and 0 <= newj2 < m:
+                                max_sum = max(max_sum,next_dp[newj1][newj2]+ cur_sum)
+                            
+                    cur_dp[j1][j2] = max_sum
+                
+            next_dp = [row[:] for row in cur_dp]
+        
+        return next_dp[0][m-1]

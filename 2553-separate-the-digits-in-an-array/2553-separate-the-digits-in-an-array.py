0ae36a8e9e0c8aa034acaf7df210e55f46303e96
Jugal Lachhwani <144345250+Jugal-lachhwani@users.noreply.github.com>
2026-05-11 10:30:49 +0530

    Time: 0 ms (100%), Space: 19.3 MB (98.35%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
@@ -2,13 +2,11 @@ class Solution:
     def separateDigits(self, nums: List[int]) -> List[int]:
         ans = []
         for num in nums:
-            r = 0
-            # d = 10
+            # r = 0
             cur = []
             while num > 0:
                 r = num % 10
                 num = num // 10
-                # d = d * 10
                 cur.append(r)
             ans.extend(cur[::-1])
         return ans

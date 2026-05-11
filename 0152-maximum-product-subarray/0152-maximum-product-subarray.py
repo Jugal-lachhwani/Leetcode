df1000491fcd3d0f2d264d32ce8c563b2f4b4c40
Jugal Lachhwani <144345250+Jugal-lachhwani@users.noreply.github.com>
2026-05-11 16:23:55 +0530

    Time: 25 ms (5.14%), Space: 23.1 MB (5.06%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -3,38 +3,38 @@ class Solution:
         n = len(arr)
         if n == 1:
             return arr[0]
-        max_num = float('-inf')
+        # max_num = float('-inf')
         dp = [[-1] * 2 for i in range(n+1)]
-        if arr[0] < 0:
-            dp[0][1] = 1
-            dp[0][0] = arr[0]
-            first_neg = True     
-        elif arr[0] > 0:
-            dp[0][1] = arr[0]
-            dp[0][0] = arr[0]
-            first_neg = False
-        else:
-            dp[0][1] = 1
-            dp[0][0] = 1
-            first_neg = False
-        # dp[0][0] = arr[0]
-        # dp[0][1] = 1
-        max_num = arr[0]  
-        for i in range(1,n):
-            if arr[i] == 0:
+        # if arr[0] < 0:
+        #     dp[0][1] = 1
+        #     dp[0][0] = arr[0]
+        #     first_neg = True     
+        # elif arr[0] > 0:
+        #     dp[0][1] = arr[0]
+        #     dp[0][0] = arr[0]
+        #     first_neg = False
+        # else:
+        #     dp[0][1] = 1
+        #     dp[0][0] = 1
+        #     first_neg = False
+        dp[0][0] = 1
+        dp[0][1] = 1
+        max_num = float('-inf')
+        first_neg = False  
+        for i in range(1,n+1):
+            if arr[i-1] == 0:
                 dp[i][0] = 1
                 dp[i][0] = 1
                 max_num = max(0,max_num)
                 first_neg = False
-                continue
-            if arr[i] < 0 and first_neg == False:
-                dp[i][0] = dp[i-1][0] * arr[i]
+            elif arr[i-1] < 0 and first_neg == False:
+                dp[i][0] = dp[i-1][0] * arr[i-1]
                 dp[i][1] = 1
                 max_num = max(dp[i][0], max_num)
                 first_neg = True
-                continue
             else:
-                dp[i][0] = dp[i-1][0] * arr[i]
-                dp[i][1] = dp[i-1][1] * arr[i]
-            max_num = max(dp[i][0],dp[i][1],max_num,arr[i])
+                dp[i][0] = dp[i-1][0] * arr[i-1]
+                dp[i][1] = dp[i-1][1] * arr[i-1]
+                max_num = max(dp[i][0],dp[i][1],max_num,arr[i-1])
+            print(max_num)
         return max_num
